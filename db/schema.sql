fa35912522856143e33498500a6f392e02e76ed3
reanna swick <thomaskubant@gmail.com>
2021-04-23 18:31:43 -0700

    started database

---
+++ db/schema.sql
@@ -0,0 +1,20 @@
+CREATE TABLE departments (
+    id INT PRIMARY KEY,
+    name VARCHAR(30),
+)
+CREATE TABLE roles (
+    id INT PRIMARY KEY,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES departments(id)
+)
+CREATE TABLE employees (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    manager_id INT,
+    FOREIGN KEY (manager_id) REFERENCES employees(id)
+)
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const inquier = require('inquirer');
-const { listenerCount } = require('node:events');
+const mysql = require('mysql2');
+
+let viewDepartments = () => {
+    
+}
 let initialize = () => {
     console.log(`
     :::::::::: ::::    ::::  :::::::::  :::        ::::::::  :::   ::: :::::::::: :::::::::: 
@@ -24,6 +28,14 @@ let initialize = () => {
             message: 'What would you like to do?',
             choices: ["View All Departments", "View All Roles", "View All Employees", "Add An Employee", "Add A Role", "Add A Department", "Update Employee Role"]
         }
-    ])
+    ]).then(answers => {
+        if(answers.userAction == 'View All Departments') {viewDepartments()}
+        if(answers.userAction == 'View All Roles') {viewRoles()}
+        if(answers.userAction == 'View All Employees') {viewEmployees()}
+        if(answers.userAction == 'Add An Employee') {addEmployee()}
+        if(answers.userAction == 'Add A Role') {addRole()}
+        if(answers.userAction == 'Add A Department') {addDepartment()}
+        if(answers.userAction == 'Update Employee Role') {updateEmployeeRole()}
+    })
 }
 initialize();
