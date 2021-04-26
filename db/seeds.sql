504d36dedcd9b86963c934d03471648811fcac63
ThomasKubant <77358241+ThomasKubant@users.noreply.github.com>
2021-04-25 21:52:50 -0700

    finished

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,27 +1,36 @@
 INSERT INTO departments (department_name)
 VALUES
-    ('OGP'),
-    ('Arts & Crafts'),
-    ('Grocery'),
-    ('Produce'),
-    ('Bakery'),
-    ('Deli'),
-    ('Hardware'),
-    ('Electronics');
+    ('Management'),
+    ('Sales'),
+    ('Customer Service'),
+    ('IT'),
+    ('Relations'),
+    ('Human Resources'),
+    ('Engineering'),
+    ('Housekeeping');
 INSERT INTO roles (title, salary, department_id)
 VALUES
-    ('Team Lead', 170000, 1),
-    ('Coach', 125000, 2),
-    ('Stocking', 100000, 3),
-    ('Digital', 110000, 8),
-    ('Cashier', 75000, 2);
+    ('District Manager', 170000, 1),
+    ('General Manager', 125000, 1),
+    ('Salesperson', 75000, 2),
+    ('Customer Service', 75000, 3),
+    ('IT', 100000, 4),
+    ('Public Relations', 80000, 5),
+    ('Human Resources Manager', 100000, 6),
+    ('Software Engineer', 100000, 7),
+    ('Engineer', 110000, 7),
+    ('Maintenence', 50000, 8);
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
     ('Thomas', 'Kubant', 1, NULL),
-    ('Test', 'Name', 2, 1),
+    ('Larry', 'Lobster', 2, 1),
     ('Christina', 'Williams', 3, 2),
     ('Merriam', 'Louis', 4, 2),
-    ('Trinity', 'Bonnette', 4, 2),
-    ('Jim', 'Carrey', 5, NULL);
+    ('Trinity', 'Bonnette', 5, 2),
+    ('Jim', 'Carrey', 6, NULL),
+    ('Barry', 'Gilmore', 7, 1),
+    ("Jerry", "Seinfeld", 8, 2),
+    ("Jake", "Ewald", 9, 2),
+    ("Brendan", "Lukens", 10, 2);
 
 
--- index.js
+++ index.js
@@ -1,19 +1,29 @@
 const inquier = require('inquirer');
 const connection = require('./db/connection.js');
 const cTable = require('console.table');
-const db = require('./db/connection.js');
+const { query } = require('./db/connection.js');
 let viewDepartments = () => {
-    let query = `SELECT department_name FROM departments`;
+    let query = `SELECT * FROM departments`;
     dbQuery(query);
     userActionPrompt();
 }
 let viewRoles = () => {
-    let query = `SELECT title FROM roles`;
+    let query = `
+    SELECT roles.id, roles.title, roles.salary, departments.department_name
+    FROM roles
+    LEFT JOIN departments
+    ON roles.department_id = departments.id;`;
     dbQuery(query);
     userActionPrompt();
 }
 let viewEmployees = () => {
-    let query = `SELECT first_name, last_name FROM employees`
+    let query = `
+    SELECT e.id, e.first_name, e.last_name, r.title, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager_name
+    FROM employees e
+    LEFT JOIN roles r
+    ON e.role_id = r.id
+    INNER JOIN employees m
+    ON m.id = e.manager_id;`
     dbQuery(query);
     userActionPrompt();
 }
@@ -23,13 +33,164 @@ let addEmployee = () => {
             name: 'firstName',
             type: 'input',
             message: "What is the employee's first name?"
+        },
+        {
+            name: 'lastName',
+            type: 'input',
+            message: "What is the employee's last name?"
+        },
+        {
+            name: 'role',
+            type: 'list',
+            message: "What is the employee's role?",
+            choices: [
+                "District Manager",
+                "General Manager",
+                "Salesperson",
+                "Customer Service Representative",
+                "IT",
+                "Public Relations",
+                "Human Resources Manager",
+                "Software Engineer",
+                "Engineer",
+                "Maintenence"
+            ]
+        },
+        {
+            name: 'manager',
+            type: 'list',
+            message: "Who is the employee's manager?",
+            choices: [
+                "Thomas Kubant",
+                "Larry Lobster"
+            ]
         }
-    ])
+    ]).then(({ firstName, lastName, role, manager }) => {
+        if (role == "District Manager") role = 1;
+        if (role == "General Manager") role = 2;
+        if (role == "Salesperson") role = 3;
+        if (role == "Customer Service Representative") role = 4;
+        if (role == "IT") role = 5;
+        if (role == "Public Relations") role = 6;
+        if (role == "Human Resources Manager") role = 7;
+        if (role == "Software Engineer") role = 8;
+        if (role == "Engineer") role = 9;
+        if (role == "Maintenence") role = 10;
+        if (manager == "Thomas Kubant") manager = 1;
+        if (manager == "Larry Lobster") manager = 2;
+        let query = `INSERT INTO employees (first_name, last_name, role_id, manager_id)
+        VALUES ('${firstName}', '${lastName}', ${role}, ${manager})`
+        dbQueryNoTable(query);
+        userActionPrompt();
+    })
+}
+let addRole = () => {
+    inquier.prompt([
+        {
+            name: 'name',
+            type: 'input',
+            message: 'What is the title of the role?'
+        },
+        {
+            name: 'salary',
+            type: 'number',
+            message: "What is the role's salary?"
+        },
+        {
+            name: 'department',
+            type: 'list',
+            message: 'What is the department the role belongs to?',
+            choices: [
+                "Management",
+                "Sales",
+                "Customer Service",
+                "IT",
+                "Relations",
+                "Human Resources",
+                "Engineering",
+                "Housekeeping"
+            ]
+        }
+    ]).then(({ name, salary, department }) => {
+        if(department == "Management") department = 1;
+        if(department == "Sales") department = 2;
+        if(department == "Customer Service") department = 3;
+        if(department == "IT") department = 4;
+        if(department == "Relations") department = 5;
+        if(department == "Human Resources") department = 6;
+        if(department == "Engineering") department = 7;
+        if(department == "Housekeeping") department = 8;
+        
+        let query = `INSERT INTO roles (title, salary, department_id)
+        VALUES
+        ('${name}', ${salary}, ${department});`
+        dbQueryNoTable(query);
+        userActionPrompt();
+    })
+}
+let addDepartment = () => {
+    inquier.prompt([
+        {
+            name: 'name',
+            type: 'input',
+            message:'What is the name of the department?'
+        },
+    ]).then(({ name }) => {
+        let query = `INSERT INTO departments (department_name)
+        VALUES
+        ('${name}')`
+        dbQueryNoTable(query);
+    })
+}
+let updateEmployeeRole = () => {
+    inquier.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: "What is the employee's last name?"
+        },
+        {
+            name: "role",
+            type: 'list',
+            message: "What is the employee's new role?",
+            choices: [
+                "District Manager",
+                "General Manager",
+                "Salesperson",
+                "Customer Service Representative",
+                "IT",
+                "Public Relations",
+                "Human Resources Manager",
+                "Software Engineer",
+                "Engineer",
+                "Maintenence"
+            ]
+        },
+    ]).then(({ name, role }) => {
+        if (role == "District Manager") role = 1;
+        if (role == "General Manager") role = 2;
+        if (role == "Salesperson") role = 3;
+        if (role == "Customer Service Representative") role = 4;
+        if (role == "IT") role = 5;
+        if (role == "Public Relations") role = 6;
+        if (role == "Human Resources Manager") role = 7;
+        if (role == "Software Engineer") role = 8;
+        if (role == "Engineer") role = 9;
+        if (role == "Maintenence") role = 10;
+        let query = `UPDATE employees SET role_id = ${role} WHERE last_name = '${name}'`;
+        dbQueryNoTable(query);
+        userActionPrompt();
+    })
 }
 let dbQuery = (query) => {
     connection.promise().query(query).then(([results]) => {
+        console.log('\n')
         console.table(results);
+        console.log('\n')
     });
+};
+let dbQueryNoTable = (query) => {
+    connection.promise().query(query)
 };
 let userActionPrompt = () => {
     inquier.prompt([
@@ -50,6 +211,9 @@ let userActionPrompt = () => {
 }
 let initialize = () => {
     console.log(`
+    ========================================================================================   
+    ========================================================================================
+
     :::::::::: ::::    ::::  :::::::::  :::        ::::::::  :::   ::: :::::::::: :::::::::: 
     :+:        +:+:+: :+:+:+ :+:    :+: :+:       :+:    :+: :+:   :+: :+:        :+:        
     +:+        +:+ +:+:+ +:+ +:+    +:+ +:+       +:+    +:+  +:+ +:+  +:+        +:+        
@@ -63,7 +227,10 @@ let initialize = () => {
     +#+  +:+  +#+ +#++:++#++: +#+ +:+ +#+ +#++:++#++: :#:        +#++:++#   +#++:++#:        
     +#+       +#+ +#+     +#+ +#+  +#+#+# +#+     +#+ +#+   +#+# +#+        +#+    +#+       
     #+#       #+# #+#     #+# #+#   #+#+# #+#     #+# #+#    #+# #+#        #+#    #+#       
-    ###       ### ###     ### ###    #### ###     ###  ########  ########## ###    ###       
+    ###       ### ###     ### ###    #### ###     ###  ########  ########## ###    ###  
+
+    ========================================================================================   
+    ========================================================================================
     `);
     userActionPrompt();
 }
